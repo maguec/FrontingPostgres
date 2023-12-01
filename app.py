292e604c7d17751f2936970ca357f3cf747945a7
Chris Mague <github@mague.com>
2023-12-01 11:42:03 -0800

    add redis caching switch

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ from fastapi.responses import JSONResponse
 from typing import Optional
 from sqlmodel import Field, SQLModel, Session, create_engine, select
 from faker import Faker
-import os, datetime
+import os, datetime, redis, pickle
 
 app = FastAPI()
 dburl = os.environ.get('DB_URL', "postgresql://postgres:PgDbFTW15@localhost:5432/profiles")
+redis_host = os.environ.get('REDIS_HOST', "localhost")
+redis_port = os.environ.get('REDIS_PORT', "6379")
+redis_pass = os.environ.get('REDIS_PASS', None)
 
 engine = create_engine(dburl)
+r = redis.StrictRedis(
+        host=redis_host,
+        password=redis_pass,
+        port=int(redis_port),
+        )
 
 ##
 class Profile(SQLModel, table=True):
@@ -29,13 +37,18 @@ async def root():
 
 @app.get("/profile/{profile_id}")
 async def read_profile(profile_id):
-    data = {}
-    with Session(engine) as session:
-        statement=select(Profile).where(Profile.id == profile_id)
-        results = session.exec(statement)
-        for result in results:
-            data = result
-    return JSONResponse(jsonable_encoder(data))
+    if r.exists('ENABLE_CACHING'):
+        data=r.get("user:{}".format(profile_id))
+        return JSONResponse(jsonable_encoder(pickle.loads(data)))
+
+    else:
+        data = {}
+        with Session(engine) as session:
+            statement=select(Profile).where(Profile.id == profile_id)
+            results = session.exec(statement)
+            for result in results:
+                data = result
+        return JSONResponse(jsonable_encoder(data))
 
 @app.get("/load")
 async def loaddata():
@@ -46,8 +59,8 @@ async def loaddata():
             fn=fake.first_name()
             ln=fake.last_name()
             dom=fake.domain_name()
-            session.add(
-                    Profile(
+            profile = Profile(
+                        id=x,
                         first_name=fn,
                         last_name=ln,
                         email="{0}_{1}@{2}".format(fn, ln, dom).lower(),
@@ -56,7 +69,8 @@ async def loaddata():
                         phone=fake.phone_number(),
                         dob=fake.date_between(),
                         )
-                    )
+            r.set("user:{}".format(x), pickle.dumps(profile))
+            session.add(profile)
             if x % 100 == 0:
                 session.commit()
         session.commit()
