229bd2ebb70f0df334837c3fb269cb0da4d4b069
Chris Mague <github@mague.com>
2023-12-08 16:01:34 -0800

    better error handling

--- api/lookup.go
+++ api/lookup.go
@@ -11,12 +11,12 @@ func Lookup(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "could not conver id to integer"})
-	} else {
-	c.JSON(http.StatusOK, gin.H{
-		"setup": c.MustGet("setup").(string),
-		"id":    id,
-	})
-}
+		return
+	} 
+		c.JSON(http.StatusOK, gin.H{
+			"setup": c.MustGet("setup").(string),
+			"id":    id,
+		})
 }
 
 func Dataload(c *gin.Context) {
